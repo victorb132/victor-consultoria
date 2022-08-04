83c4860ab2f2c8d5bb0769553b1a5953a4e1732f
Victor Novais <victor@digitalrepublic.com.br>
2022-08-04 17:54:14 -0300

    feature/neon-text

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
 import { GlobalStyle } from './styles/global';
+import styled from 'styled-components';
+
+const NeonText = styled.p`
+  color: #fff;
+  text-shadow:
+    0 0 7px #fff,
+    0 0 10px #fff,
+    0 0 21px #fff,
+    0 0 42px #000,
+    0 0 82px #000,
+    0 0 92px #000,
+    0 0 102px #000,
+    0 0 151px #000;
+  align-self: center;
+  font-size: 50px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+  background-color: black;
+`;
+
 
 export function App() {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      height: '100vh',
-      flexDirection: 'column',
-      backgroundColor: '#ff9e80'
-    }}>
-        <p style={{
-          fontSize: 50,
-          alignSelf: 'center',
-        }}>
+    <Container>
+        <NeonText>
           Este site encontra-se em desenvolvimento
-        </p>
-        <p style={{
-          fontSize: 20,
-          alignSelf: 'center',
-        }}>
+        </NeonText>
+        <NeonText style={{ fontSize: 20}}>
           para contato: victor96novais@gmail.com
-        </p>
+        </NeonText>
       <GlobalStyle />
-    </div>
+    </Container>
   )
 }
 
