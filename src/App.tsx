376172a0694d30dfb8b58efa93441206b7497a1e
Victor Novais <victor@digitalrepublic.com.br>
2022-08-04 17:38:30 -0300

    feature/dev-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      flexDirection: 'column',
+      backgroundColor: '#ff9e80'
+    }}>
+        <p style={{
+          fontSize: 50,
+          alignSelf: 'center',
+        }}>
+          Este site encontra-se em desenvolvimento
+        </p>
+        <p style={{
+          fontSize: 20,
+          alignSelf: 'center',
+        }}>
+          para contato: victor96novais@gmail.com
+        </p>
+      <GlobalStyle />
+    </div>
   )
 }
 
