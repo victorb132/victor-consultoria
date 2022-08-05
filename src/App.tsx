0a727398843b6002ffea694b06fd666374b4ff9e
Victor Novais <victor@digitalrepublic.com.br>
2022-08-05 14:51:09 -0300

    feature/theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
-import styled from 'styled-components';
 
-const NeonText = styled.p`
-  color: #fff;
-  text-shadow:
-    0 0 7px #fff,
-    0 0 10px #fff,
-    0 0 21px #fff,
-    0 0 42px #000,
-    0 0 82px #000,
-    0 0 92px #000,
-    0 0 102px #000,
-    0 0 151px #000;
-  align-self: center;
-  font-size: 50px;
-  text-align: center;
-`;
+import { Home } from './presentation/home';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-  flex-direction: column;
-  background-color: black;
-  padding: 10px;
-`;
-
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <Container>
-        <NeonText>
-          Este site encontra-se em desenvolvimento
-        </NeonText>
-        <NeonText style={{ fontSize: 20}}>
-          para contato: victor96novais@gmail.com
-        </NeonText>
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
       <GlobalStyle />
-    </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/presentation/home/index.tsx
@@ -0,0 +1,34 @@
+
+import styled from 'styled-components';
+
+const NeonText = styled.p`
+  color: #fff;
+  align-self: center;
+  font-size: 50px;
+  text-align: center;
+  opacity: 0.5;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+
+export function Home() {
+  return (
+    <Container>
+        <NeonText>
+          Este site encontra-se em desenvolvimento
+        </NeonText>
+        <NeonText style={{ fontSize: 20}}>
+          para contato: victor96novais@gmail.com
+        </NeonText>
+    </Container>
+  )
+}
+
+
