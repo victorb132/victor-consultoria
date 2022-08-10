bbc818272e6d5e32627ce4d979f6a487f8847ba6
Victor <victor96novais@gmail.com>
2022-08-10 09:35:45 -0300

    feature/prismic

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { App } from './App'
+
+import { PrismicProvider } from '@prismicio/react'
+import { client } from './services/prismic'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrismicProvider client={client}>
+      <App />
+    </PrismicProvider>
   </React.StrictMode>,
 )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,45 @@
+import { useAllPrismicDocumentsByType } from '@prismicio/react'
+import { useEffect, useState } from 'react'
 import { Container, Title, Subtitle } from './styles'
 
+interface HomePrismic {
+  data: {
+    title: [
+      {
+        text: string
+      },
+    ]
+    subtitle: [
+      {
+        text: string
+      },
+    ]
+  }
+}
+
 export function Home() {
+  const [documents, { state, error }] = useAllPrismicDocumentsByType('home')
+  const [header, setHeader] = useState({
+    title: '',
+    subTitle: '',
+  })
+
+  console.log('result', state)
+  console.log('result', error)
+
+  useEffect(() => {
+    if (state === 'loaded') {
+      setHeader({
+        title: documents[0].data.title[0].text,
+        subTitle: documents[0].data.subtitle[0].text,
+      })
+    }
+  }, [documents, state])
+
   return (
     <Container>
-      <Subtitle>Opa, sou o</Subtitle>
-      <Title>Victor Novais</Title>
+      <Subtitle>{header.subTitle}</Subtitle>
+      <Title>{header.title}</Title>
     </Container>
   )
 }
